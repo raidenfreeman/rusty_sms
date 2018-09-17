aa93b10781372fdc196403117dfb43bdfce98db2
Theodoros Chatzigiannakis <tchatzigiannakis@gmail.com>
2018-09-17 15:01:07 +0300

    Added general-purpose bitwise_operation().

--- src/vm/machine.rs
+++ src/vm/machine.rs
@@ -226,6 +226,21 @@ impl Machine {
         }
     }
 
+    fn bitwise_operation(&mut self, operand: u8, operation: fn(u8, u8) -> u8) {
+        let op1 = self.cpu.state.registers.a;
+        let op2 = operand;
+        let result = operation(op1, op2);
+        let parity = (0..8).fold(0, |acc, b| { acc + (result >> b) & 1 }) % 2 == 0;
+
+        let status = &mut self.cpu.state.status;
+        Flag::ParityOverflow.set(status, parity);
+        Flag::Carry.set(status, false);
+        Flag::HalfCarry.set(status, false);
+        Flag::AddSubtract.set(status, false);
+        Flag::Zero.set(status, result == 0x00);
+        Flag::Sign.set(status, result > 0x7F);
+    }
+
     fn load_into_register(&mut self, selector: fn(&mut Registers) -> (&mut u8, &mut u8)) {
         {
             let address = self.next_word();
