62026301ac6957ec85f9259908ca1766cc4a3ea2
Theodoros Chatzigiannakis <tchatzigiannakis@gmail.com>
2018-10-22 17:14:36 +0300

    Corrected RLCA.

--- src/vm/instructions/bitwise.rs
+++ src/vm/instructions/bitwise.rs
@@ -40,10 +40,10 @@ impl Machine {
 
     pub(crate) fn rotate_accumulator_left(&mut self) {
         let old_value = self.cpu.state.registers.a as u16;
-        let old_carry = Flag::Carry.get_bit(&mut self.cpu.state.status) as u16;
-        let new_value = (old_value << 1) + old_carry;
-        let new_carry = new_value & 0x100 != 0;
-        Flag::Carry.set(&mut self.cpu.state.status, new_carry);
+        let second_most_significant_bit = if old_value & 0x40 != 0 { 1 } else { 0 }; 
+        let new_value = (old_value << 1) + second_most_significant_bit;
+        self.cpu.state.registers.a = new_value as u8;
+        Flag::Carry.set(&mut self.cpu.state.status, second_most_significant_bit == 1);
         Flag::HalfCarry.set(&mut self.cpu.state.status, false);
         Flag::AddSubtract.set(&mut self.cpu.state.status, false);
         self.clock(4);
