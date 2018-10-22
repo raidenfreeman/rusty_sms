b3623ab8261cdbf638ab7fbdd5761ea4f2a45b0d
Theodoros Chatzigiannakis <tchatzigiannakis@gmail.com>
2018-10-22 19:26:22 +0300

    Changed parameter name.

--- src/vm/instructions/control.rs
+++ src/vm/instructions/control.rs
@@ -10,10 +10,10 @@ impl Machine {
         self.clock(4);
     }
 
-    pub(crate) fn jump(&mut self, predicate: fn(&u8) -> bool) {
+    pub(crate) fn jump(&mut self, condition: fn(&u8) -> bool) {
         let dest = self.next_word();
 
-        if predicate(&self.cpu.state.status) {
+        if condition(&self.cpu.state.status) {
             self.cpu.goto(dest);
         }
 
